3c60dbebd13695ce05b23aa9d79bb36b6b01970e
Ambrosemilean <162729808+Ambrosemilean@users.noreply.github.com>
2026-03-08 16:16:56 +0100

    Create app_constants.dart

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  static const String appName = "Ambrose Milean Enterprise";
+  static const String companyName = "Ambrose Milean Enterprise";
+  static const String companyAddress = "Your Business Address";
+  static const String companyPhone = "+1234567890";
+  static const String companyEmail = "[email protected]";
+  static const String taxRate = "10.0"; // 10% tax
+  static const String currencySymbol = "\$";
+  
+  static const Color primaryColor = Color(0xFF2A4B7C); // Brand color
+  static const Color secondaryColor = Color(0xFFC49A6C);
+}
